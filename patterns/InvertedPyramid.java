3b8bd39c8c942387a49c104975d51cf7a0faf22e
Nayan <nayandahare02@gmail.com>
2026-05-01 06:03:01 +0530

    patterns 1/05/2026

---
+++ patterns/InvertedPyramid.java
@@ -0,0 +1,17 @@
+public class InvertedPyramid {
+    public static void main(String[] args) {
+        int n = 6;
+        for(int row = 1 ; row <= n ; row++)
+        {
+            for(int col = 1 ; col <= row - 1; col++ )
+            {
+                System.out.print("  ");
+            }
+            for(int col = 1 ; col <= 2* n - 2*row + 1; col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

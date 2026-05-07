f0ec76e3e1a0284914275809689fe6a1b850e6bd
Nayan <nayandahare02@gmail.com>
2026-05-07 20:23:43 +0530

    SECOND-LARGEST PROB 7/05/2026

--- twoPointer/SecondLargestElement.java
+++ twoPointer/SecondLargestElement.java
@@ -3,15 +3,15 @@ import java.util.Arrays;
 public class SecondLargestElement {
     public static void main(String[] args) {
         int [] arr={10,11, 5, 8, 19, 20};
-        int n = arr.length - 1 ;
-        int secondLargest = arr[0];
-        Arrays.sort(arr);
+        int n = arr.length - 1 ; // firsst of sort array length in 'n' variable
+        int secondLargest = arr[0]; // sL var give 1st element
+        Arrays.sort(arr); // sort the array
         
         for(int i = 1 ; i <= n ; i ++)
         {
-            if(secondLargest < arr[i])
+            if(secondLargest < arr[i]) // com. 1st ele to all ele of array , then we get max
             {
-                secondLargest = arr[i-1];
+                secondLargest = arr[i-1]; // after getinng max ele just sort in sL wit -1 , and get seconf largest elemt bcoz array is sorted
             }
         }
         System.out.println("Second largest -->>> " + secondLargest);
